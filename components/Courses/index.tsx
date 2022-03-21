1be7be87b636b3b8e423b80036aa8436188361b1
Albin Shrestha <shrestha_albin@yahoo.com>
2022-03-21 10:31:48 -0500

    pages added

---
+++ components/Courses/index.tsx
@@ -0,0 +1,3 @@
+import CoursesPage from "./Courses";
+
+export default CoursesPage;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import LandingPage from "../components/Landing";
+
+const Home: NextPage = () => {
+  return <LandingPage />;
+};
+
+export default Home;
